088fc25583ae28cd8fcf53a0a923c18382991fd7
Juho Lehtonen <mr.meitsi@hotmail.com>
2018-01-11 08:52:46 +0200

    T2

---
+++ Demo02/T2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna pisteet: ");
+            int pisteet = int.Parse(Console.ReadLine());
+            switch (pisteet)
+            {
+                case 0: Console.WriteLine("Numerosi on 0"); break;
+                case 2: Console.WriteLine("Numerosi on 1"); break;
+                case 3: Console.WriteLine("Numerosi on 1"); break;
+            }
+            }
+        }
+    }
+}
