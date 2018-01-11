a983bda7f503dd31f13c56ec627bf67445fa9b68
Juho Lehtonen <mr.meitsi@hotmail.com>
2018-01-11 08:54:39 +0200

    valmis

--- Demo02/T2/Program.cs
+++ Demo02/T2/Program.cs
@@ -15,10 +15,19 @@ namespace T2
             switch (pisteet)
             {
                 case 0: Console.WriteLine("Numerosi on 0"); break;
+                case 1: Console.WriteLine("Numerosi on 0"); break;
                 case 2: Console.WriteLine("Numerosi on 1"); break;
                 case 3: Console.WriteLine("Numerosi on 1"); break;
+                case 4: Console.WriteLine("Numerosi on 2"); break;
+                case 5: Console.WriteLine("Numerosi on 2"); break;
+                case 6: Console.WriteLine("Numerosi on 3"); break;
+                case 7: Console.WriteLine("Numerosi on 3"); break;
+                case 8: Console.WriteLine("Numerosi on 4"); break;
+                case 9: Console.WriteLine("Numerosi on 4"); break;
+                case 10: Console.WriteLine("Numerosi on 5"); break;
+                case 11: Console.WriteLine("Numerosi on 5"); break;
+                case 12: Console.WriteLine("Numerosi on 5"); break;
             }
             }
         }
     }
-}
